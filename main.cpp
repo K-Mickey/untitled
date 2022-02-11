b1d06dafd426c6d4fd91326ca2bc3aa64b60c3cd
K-Mickey <82704494+K-Mickey@users.noreply.github.com>
2022-02-11 15:34:48 +0300

    A few cosmetic details

--- main.cpp
+++ main.cpp
@@ -9,6 +9,15 @@
 
 using namespace std;
 
+class Date;
+class Database;
+
+Date ParseDate(const string&); //Обработка даты из входного потокаx
+bool operator<(const Date&, const Date&);//Перезагрузка оператора сравнения для сравнения дат
+ostream& operator<<(ostream&, const Date&); //Перезагрузка оператора вывода для даты
+ofstream& operator<<(ofstream&, const Database&); //Перезагрузка для чтения БД из файла
+
+
 // Класс для хранения даты и обработки основных логических ошибок (количество месяцев в году и дней в месяце)
 class Date {
 public:
@@ -42,32 +51,31 @@ private:
     int day;
 };
 
-//Перезагрузка оператора сравнения для сравнения дат
-bool operator<(const Date& lhs, const Date& rhs) {
-    return vector<int>{lhs.GetYear(), lhs.GetMonth(), lhs.GetDay()} <
-           vector<int>{rhs.GetYear(), rhs.GetMonth(), rhs.GetDay()};
-}
-
-//Перезагрузка оператора вывода для даты
-ostream& operator<<(ostream& stream, const Date& date) {
-    stream << setw(4) << setfill('0') << date.GetYear() <<
-           "-" << setw(2) << setfill('0') << date.GetMonth() <<
-           "-" << setw(2) << setfill('0') << date.GetDay();
-    return stream;
-}
-
 
 //Класс для организации и управления простой БД
 class Database {
 public:
+
     //Добавление нового элемента в БД
-    void AddEvent(const Date& date, const string& event) {
+    void AddEvent(stringstream& str) {
+
+        string date_str, event;
+        str >> date_str;
+        const Date date = ParseDate(date_str);
+
+        while (!str.eof()) {
+            string n;
+            str >> n;
+            event += n + " ";
+        }
+
         storage[date].insert(event);
     }
 
     //Удаление только события из БД, если такое имеется
     //Возврат True при удалении
     bool DeleteEvent(const Date& date, const string& event) {
+
         if (storage.count(date) > 0 && storage[date].count(event) > 0) {
             storage[date].erase(event);
             return true;
@@ -78,6 +86,7 @@ public:
     //Удаление даты вместе со всеми её событиями
     //Возврат количества удаленных событий
     int DeleteDate(const Date& date) {
+
         if (storage.count(date) == 0) {
             return 0;
         } else {
@@ -89,6 +98,7 @@ public:
 
     //Поиск событий по дате
     set<string> Find(const Date& date) const {
+
         if (storage.count(date) > 0) {
             return storage.at(date);
         } else {
@@ -98,28 +108,148 @@ public:
 
     //Печать всей БД
     void Print() const {
+
         for (const auto& item : storage) {
             for (const string& event : item.second) {
                 cout << item.first << " " << event << endl;
             }
         }
+
+        if (storage.empty()) {
+            cout << "Database is empty" << endl;
+        }
+
+    }
+
+    //Удалить всё из БД
+    void DeleteAll() {
+        storage.clear();
     }
 
     friend ofstream& operator<<(ofstream&, const Database&);
+
 private:
     map<Date, set<string>> storage;
 };
 
-//Перезагрузка для чтения БД из файла
-ofstream& operator<<(ofstream& stream, const Database& db)  {
-    for (const auto& item : db.storage) {
-        for (const string& event : item.second) {
-            stream << item.first << " " << event << endl;
+
+int main() {
+
+    Database db;
+
+    cout << "Please, enter a command: Add, Del, Find, Print, Save, Read, Clean, End" << endl <<
+         "Save, Read, Clear - these are commands for working with a file" << endl <<
+         "Special format for input:" << endl <<
+         "Add 0000-01-01 Event" << endl <<
+         "Del all, Del 0000-01-01 or Del 0000-01-01 Event" << endl <<
+         "Find 0000-01-01" << endl;
+
+    string command_line;
+    while (getline(cin, command_line)) {
+
+        try {
+
+            stringstream ss(command_line);
+            string command;
+            ss >> command;
+
+            if (command == "Add") { //Добавление события
+
+                db.AddEvent(ss);
+
+            } else if (command == "Del") { //Удаление события
+
+                string date_str;
+                ss >> date_str;
+                if (date_str == "all") {
+                    db.DeleteAll();
+                    cout << "The database has been delete" << endl;
+                    continue;
+                }
+
+                string event;
+                if (!ss.eof()) { //Проверка на наличие события
+                    ss >> event;
+                }
+
+                const Date date = ParseDate(date_str);
+
+                if (event.empty()) { //Удаляем все события, если указана только дата
+                    const int count = db.DeleteDate(date);
+                    cout << "Deleted " << count << " events" << endl;
+                } else {
+                    if (db.DeleteEvent(date, event)) {
+                        cout << "Deleted successfully" << endl;
+                    } else {
+                        cout << "Event not found" << endl;
+                    }
+                }
+
+            } else if (command == "Find") { //Поиск событий по дате
+
+                string date_str;
+                ss >> date_str;
+                const Date date = ParseDate(date_str);
+                for (const string &event: db.Find(date)) {
+                    cout << event << endl;
+                }
+
+            } else if (command == "Print") { //Вывод БД
+
+                db.Print();
+
+            } else if (command == "Save") { //Сохранение БД на ЖД
+
+                ofstream fout("save_bd.txt", ios::app);
+                if(!fout) {
+                    throw logic_error("The file if not open");
+                }
+                fout << db;
+                fout.close();
+                cout << "Saving completed" << endl;
+
+            } else if (command == "Read") { //Считывание сохраненных ранее событий
+
+                ifstream inf("save_bd.txt");
+                if(!inf) {
+                    throw logic_error("The file if not open");
+                }
+
+                string line;
+                while(getline(inf, line)) {
+                    stringstream str(line);
+                    cout << line << endl;
+                    db.AddEvent(str);
+                }
+                inf.close();
+
+            } else if (command == "Clear"){ //Очистка файла с БД
+
+                ofstream fout("save_bd.txt");
+                if(!fout) {
+                    throw logic_error("The file if not open");
+                }
+                fout.close();
+                cout << "The file is cleared" << endl;
+
+            } else if (command == "End") { //Завершение программы
+
+                    cout << "Goodbye!";
+                    break;
+
+            } else if (!command.empty()) {
+
+                throw logic_error("Unknown command: " + command);
+
+            }
+        }
+        catch (const exception &e) {
+            cout << e.what() << endl;
         }
     }
-    return stream;
-}
 
+    return 0;
+}
 
 //Обработка даты из входного потока
 //Вывод ошибки при некорректном вводе
@@ -147,122 +277,25 @@ Date ParseDate(const string& date) {
     return Date(year, month, day);
 }
 
+//Перезагрузка оператора сравнения для сравнения дат
+bool operator<(const Date& lhs, const Date& rhs) {
+    return vector<int>{lhs.GetYear(), lhs.GetMonth(), lhs.GetDay()} <
+           vector<int>{rhs.GetYear(), rhs.GetMonth(), rhs.GetDay()};
+}
 
-void Add (Database& db, stringstream& str){
-    string date_str, event;
-    str >> date_str;
-    while (!str.eof()) {
-        string n;
-        str >> n;
-        event += n + " ";
-    }
-    const Date date = ParseDate(date_str);
-    db.AddEvent(date, event);
-};
-
-int main() {
-    Database db;
-
-    string command_line;
-
-    cout << "Please, enter a command: Add, Del, Find, Print, Save, Read, Clean, End" << endl <<
-         "Save, Read, Clean - these are commands for working with a file" << endl <<
-         "Special format for input:" << endl << "Add 0000-01-01 Event" << endl <<
-         "Del 0000-01-01 or Del 0000-01-01 Event" << endl <<
-         "Find 0000-01-01" << endl;
-
-    while (getline(cin, command_line)) {
-
-        try {
-            stringstream ss(command_line);
-
-            string command;
-            ss >> command;
-
-            if (command == "Add") {
-
-                Add(db, ss);
-
-            } else if (command == "Del") {
-
-                string date_str;
-                ss >> date_str;
-                string event;
-                if (!ss.eof()) {
-                    ss >> event;
-                }
-                const Date date = ParseDate(date_str);
-                if (event.empty()) {
-                    const int count = db.DeleteDate(date);
-                    cout << "Deleted " << count << " events" << endl;
-                } else {
-                    if (db.DeleteEvent(date, event)) {
-                        cout << "Deleted successfully" << endl;
-                    } else {
-                        cout << "Event not found" << endl;
-                    }
-                }
-
-            } else if (command == "Find") {
-
-                string date_str;
-                ss >> date_str;
-                const Date date = ParseDate(date_str);
-                for (const string &event: db.Find(date)) {
-                    cout << event << endl;
-                }
-
-            } else if (command == "Print") {
-
-                db.Print();
-
-            } else if (command == "Save") {
-
-                ofstream fout("save_bd.txt", ios::app);
-                if(!fout) {
-                    throw logic_error("The file if not open");
-                }
-                fout << db;
-                fout.close();
-
-            } else if (command == "Read") {
-
-                ifstream inf("save_bd.txt");
-                if(!inf) {
-                    throw logic_error("The file if not open");
-                }
-
-                string line;
-                while(getline(inf, line)) {
-                    stringstream str(line);
-                    cout << line << endl;
-                    Add(db,str);
-                }
-                inf.close();
-
-            } else if (command == "Clean"){
-
-                ofstream fout("save_bd.txt");
-                if(!fout) {
-                    throw logic_error("The file if not open");
-                }
-                fout.close();
-
-            } else if (command == "End") {
-
-                    cout << "Goodbye!";
-                    break;
-
-            } else if (!command.empty()) {
-
-                throw logic_error("Unknown command: " + command);
-
-            }
-        }
-        catch (const exception &e) {
-            cout << e.what() << endl;
+//Перезагрузка оператора вывода для даты
+ostream& operator<<(ostream& stream, const Date& date) {
+    stream << setw(4) << setfill('0') << date.GetYear() << "-"
+            << setw(2) << setfill('0') << date.GetMonth() << "-"
+            << setw(2) << setfill('0') << date.GetDay();
+    return stream;
+}
+//Перезагрузка для чтения БД из файла
+ofstream& operator<<(ofstream& stream, const Database& db)  {
+    for (const auto& item : db.storage) {
+        for (const string& event : item.second) {
+            stream << item.first << " " << event << endl;
         }
     }
-
-    return 0;
+    return stream;
 }
